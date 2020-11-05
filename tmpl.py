304cb15463ed67af5c59c13710584e8cbd34f4b5
Kyle Xie <kylexie186@gmail.com>
2020-11-05 03:03:23 -0500

    update python tmpl

--- tmpl.py
+++ tmpl.py
@@ -12,6 +12,13 @@ rints = lambda: list(map(int, input().split()))
 rstrs = lambda: input().split()
 wmat = lambda n, mat, sep: '{}\n{}'.format(n, '\n'.join(sep.join(map(str, row)) for row in mat))
 warr = lambda n, arr, sep: '{}\n{}'.format(n, sep.join(map(str, arr)))
-wl = lambda *arr: ' '.join(map(str, arr))
+wl = lambda sep, *arr: sep.join(map(str, arr))
 
+ctoi = lambda x : ord(x) - ord('a')
+itoc = lambda x : chr(x + ord('a'))
 
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
